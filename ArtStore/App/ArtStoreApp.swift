4146904a9581d69c3925eba2c5cee623d3ba9c71
albanovaka <albanova.kanykey@courrier.uqam.ca>
2024-04-03 17:39:58 -0400

    Initial Commit

---
+++ ArtStore/App/ArtStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtStoreApp.swift
+//  ArtStore
+//
+//  Created by Kanykey Albanova on 2024-04-03.
+//
+
+import SwiftUI
+
+@main
+struct ArtStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
