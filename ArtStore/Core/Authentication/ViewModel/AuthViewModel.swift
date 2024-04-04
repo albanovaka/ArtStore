89f33422f7b2bc971dbc7d435880367602e5ab48
albanovaka <albanova.kanykey@courrier.uqam.ca>
2024-04-04 18:07:55 -0400

    Retrieving user data from database as we are fetching the user

--- ArtStore/Core/Authentication/ViewModel/AuthViewModel.swift
+++ ArtStore/Core/Authentication/ViewModel/AuthViewModel.swift
@@ -44,5 +44,7 @@ class AuthViewModel: ObservableObject{
     
     func fetchUser() async{
         guard let uid = Auth.auth().currentUser?.uid else {return}
+        let snapshot = try? await Firestore.firestore() .collection("users").document(uid).getDocument()
+        
     }
 }
