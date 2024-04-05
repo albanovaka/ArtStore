8521607ec45410c334def994185d5cdf40e630cf
albanovaka <albanova.kanykey@courrier.uqam.ca>
2024-04-05 09:39:06 -0400

    ItemsGridView has been created. Please note that this was done before but due to debugging, i went a couple commits back

---
+++ ArtStore/Core/Items/ItemsGridView.swift
@@ -0,0 +1,43 @@
+//
+//  ItemsGridView.swift
+//  ArtStore
+//
+//  Created by Kanykey Albanova on 2024-04-05.
+//
+
+import SwiftUI
+
+struct ItemsGridView: View {
+    @ObservedObject var itemsViewModel = ItemsViewModel()
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+    var body: some View {
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(itemsViewModel.items) { item in
+                    VStack {
+                        AsyncImage(url: URL(string: item.image_url)) { image in
+                            image.resizable()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(10)
+                        
+                        Text(item.description)
+                            .font(.caption)
+                            .lineLimit(1)
+                    }
+                }
+            }
+        }
+        .onAppear {
+            itemsViewModel.fetchItems()
+        }
+    }
+}
+
+struct ItemsGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemsGridView()
+    }
+}
