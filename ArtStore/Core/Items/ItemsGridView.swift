e926a18d7736f7c8711f6328278359a3b322903f
albanovaka <albanova.kanykey@courrier.uqam.ca>
2024-04-05 10:39:19 -0400

    The link to the profile view works

--- ArtStore/Core/Items/ItemsGridView.swift
+++ ArtStore/Core/Items/ItemsGridView.swift
@@ -13,31 +13,43 @@ struct ItemsGridView: View {
     @EnvironmentObject var viewModel: AuthViewModel
     @StateObject var itemsViewModel = ItemsViewModel()
     let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+
     var body: some View {
-        
-        ScrollView {
-            LazyVGrid(columns: columns) {
-                ForEach(itemsViewModel.items) { item in
-                    VStack {
-                        KFImage(URL(string: item.image_url))
-                            .resizable()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(itemsViewModel.items) { item in
+                        VStack {
+                            KFImage(URL(string: item.image_url))
+                                .resizable()
                                 .scaledToFit()
                                 .frame(width: 100, height: 100)
                                 .cornerRadius(10)
-                            
-                        
-                        Text(item.description)
-                            .font(.caption)
-                            .lineLimit(1)
+
+                            Text(item.description)
+                                .font(.caption)
+                                .lineLimit(1)
+                        }
                     }
                 }
             }
-        }
-        .onAppear {
-            itemsViewModel.fetchItems()
+            .navigationTitle("Items")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: ProfileView()) {
+                        Text("Go to Profile")
+                    }
+                }
+            }
+            .onAppear {
+                itemsViewModel.fetchItems()
+            }
         }
     }
 }
+
+
+
 
 struct ItemsGridView_Previews: PreviewProvider {
     static var previews: some View {
