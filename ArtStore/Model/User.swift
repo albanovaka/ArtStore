3babe53ed11a7caebbba9d1b20efa55853c3de4c
albanovaka <albanova.kanykey@courrier.uqam.ca>
2024-04-07 19:14:25 -0400

    Updated the user to have an array of favourite item and an array for  basket items

--- ArtStore/Model/User.swift
+++ ArtStore/Model/User.swift
@@ -6,20 +6,38 @@
 //
 
 import Foundation
-struct User: Identifiable, Codable{
+struct User: Identifiable, Codable {
     let id: String
     let fullname: String
     let email: String
+    var favorites: [String] // This would be an array of favorite item IDs.
+    var basket: [BasketItem] // This could be an array of basket items with more detail.
     
-    var initials: String{
+    var initials: String {
         let formatter = PersonNameComponentsFormatter()
-        if let components = formatter.personNameComponents(from: fullname){
+        if let components = formatter.personNameComponents(from: fullname) {
             formatter.style = .abbreviated
             return formatter.string(from: components)
         }
         return ""
     }
 }
-extension User {
-    static var MOCK_USER = User(id: NSUUID().uuidString, fullname: "Bob Sinclair", email: "test@gmail.com")
+
+struct BasketItem: Codable {
+    let itemId: String
+    let quantity: Int
 }
+
+extension User {
+    static var MOCK_USER = User(
+        id: NSUUID().uuidString,
+        fullname: "Bob Sinclair",
+        email: "test@gmail.com",
+        favorites: ["item1ID", "item2ID", "item3ID"], // Mock list of favorite item IDs
+        basket: [
+            BasketItem(itemId: "item4ID", quantity: 1),
+            BasketItem(itemId: "item5ID", quantity: 2)
+        ] // Mock basket with some items and quantities
+    )
+}
+
