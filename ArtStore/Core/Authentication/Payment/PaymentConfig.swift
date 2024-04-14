3ed5039e122055bd9d9ff87bf7283dc9f3fc1675
albanovaka <albanova.kanykey@courrier.uqam.ca>
2024-04-13 21:47:59 -0400

    Payment configuration in process

---
+++ ArtStore/Core/Authentication/Payment/PaymentConfig.swift
@@ -0,0 +1,8 @@
+//
+//  PaymentConfig.swift
+//  ArtStore
+//
+//  Created by Kanykey Albanova on 2024-04-13.
+//
+
+import Foundation
---
+++ ArtStore/Core/Profile/PaymentView.swift
@@ -0,0 +1,18 @@
+//
+//  PaymentView.swift
+//  ArtStore
+//
+//  Created by Kanykey Albanova on 2024-04-13.
+//
+
+import SwiftUI
+
+struct PaymentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaymentView()
+}
